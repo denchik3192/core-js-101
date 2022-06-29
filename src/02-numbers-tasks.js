a246735fba7f4cc617ff815880534ce3e1b7ca1e
DENIS <den465341@gmail.com>
2022-06-29 22:25:56 +0300

    feat: implement isPrime func

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -201,8 +201,14 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  if (n <= 1) return false;
+  if (n % 2 === 0 && n > 2) return false;
+  const s = Math.sqrt(n);
+  for (let i = 3; i <= s; i += 2) {
+    if (n % i === 0) return false;
+  }
+  return true;
 }
 
 /**
